3b712ae473beff3dab0a84b8bd6209a01f89a038
João Pedro <irus0ubusiness@gmail.com>
2023-08-13 15:42:38 +0100

    feat: add initial html structure + header

--- README.md
+++ README.md
@@ -33,4 +33,4 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 
-- Special thank you to developer @Fernanda Kipper for the inspiration to do this project
+- Special thank you to @Fernanda Kipper for the inspiration to do this project
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,31 @@
-import Image from "next/image";
+import { Header } from "./components/header/header";
+import "./styles/home.scss";
 
 export default function Home() {
 	return (
-		<main>
-			<div className="header">
-				<Image
-					src="/vercel.svg"
-					alt="Vercel Logo"
-					width={100}
-					height={24}
-					priority
-				/>
-				<div>
-					<h1>Hi, im João!</h1>
-					<h2>Software Engineer</h2>
+		<main className="container">
+			<Header />
+			<div className="experience">
+				<h3>Experience</h3>
+				<p>texto</p>
+				<div className="experience-time">{/* TODO LANGUAGES EXPERIENCE */}</div>
+				<div className="infos">
+					<h3 className="languages"></h3>
+					<div className="languages-info">
+						<span>En - Fluent</span>
+						<span>Pt - Native Speaker</span>
+					</div>
+					<h3>Education</h3>
+					<div className="educational-info">
+						<span>🎓</span>
+						<span>
+							Engenharia informática - Instituto politécnico de Setúbal
+						</span>
+					</div>
 				</div>
-				<div className="experience">
-					<h3>Experience</h3>
-					<p>texto</p>
-					<div className="experience-time">
-						{/* TODO LANGUAGES EXPERIENCE */}
-					</div>
-					<div className="infos">
-						<h3 className="languages"></h3>
-						<div className="languages-info">
-							<span>En - Fluent</span>
-							<span>Pt - Native Speaker</span>
-						</div>
-						<h3>Education</h3>
-						<div className="educational-info">
-							<span>🎓</span>
-							<span>
-								Engenharia informática - Instituto politécnico de Setúbal
-							</span>
-						</div>
-					</div>
-					<div className="buttons">
-						<div className="social"></div>
-						<button>contact me</button>
-					</div>
+				<div className="buttons">
+					<div className="social"></div>
+					<button>contact me</button>
 				</div>
 			</div>
 		</main>
